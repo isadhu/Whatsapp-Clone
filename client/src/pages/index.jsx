7c88d9dfbff03a1906358f72064f5e8eecb06ee9
isadhu <sadhu@Sadhus-MacBook-Pro.local>
2023-07-28 02:14:35 -0700

    Removed client folder from repository

--- client/src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function index() {
-  return <div>ex</div>;
-}
-
-export default index;
--- client/src/pages/login.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function login() {
-	return <div className='flex justify-center'>login</div>;
-}
-
-export default login;
